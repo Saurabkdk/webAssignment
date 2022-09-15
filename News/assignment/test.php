483ed65f3f49113db19d6380eb90043b1912d4f9
Saurabkdk <saurabkdk@gmail.com>
2022-09-16 01:16:26 +0545

    Testing of article generator

---
+++ News/assignment/test.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="../styles.css?v=<?php echo time(); ?>"/>
+
+<?php
+include 'header.php';
+ ?>
+
+<div>
+<h1>Articles</h1>
+<div class="titleView">
+<div class="text">
+<div class="title"><a href = "#">
+<h2>Roger Federer to retire from tennis after Laver Cup aged 41</h2>
+</a></div>
+<div class="dateCategory">
+<div class="date">
+<h4>Date</h4>
+</div>
+<div class="category">
+<h4>Category</h4>
+</div>
+</div>
+<div class="editDelete">
+  <p><a id="read" href="#">Read complete article...</a></p>
+<div class="edit">
+<p><a id="edit" href="#">Edit</a></p>
+</div>
+<div class="delete">
+<p><a id="delete" href="#">Delete</a></p>
+</div>
+</div>
+</div>
+<div class="image">
+<img src="./public/images/articles/rogerfederer.jpg" alt="Roger Federer" width="500px" height="300px">
+</div>
+</div>
+</div>
+
+ <?php include 'footer.php' ?>
