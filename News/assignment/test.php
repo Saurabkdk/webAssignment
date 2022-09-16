14711ef9f95358fc8cfa1b21d2682ed135d874d1
Saurabkdk <saurabkdk@gmail.com>
2022-09-16 22:06:09 +0545

    Testing display of articles

--- News/assignment/test.php
+++ News/assignment/test.php
@@ -4,35 +4,37 @@
 include 'header.php';
  ?>
 
-<div>
-<h1>Articles</h1>
-<div class="titleView">
-<div class="text">
-<div class="title"><a href = "#">
-<h2>Roger Federer to retire from tennis after Laver Cup aged 41</h2>
-</a></div>
-<div class="dateCategory">
-<div class="date">
-<h4>Date</h4>
-</div>
-<div class="category">
-<h4>Category</h4>
-</div>
-</div>
-<div class="editDelete">
-  <p><a id="read" href="#">Read complete article...</a></p>
-<div class="edit">
-<p><a id="edit" href="#">Edit</a></p>
-</div>
-<div class="delete">
-<p><a id="delete" href="#">Delete</a></p>
-</div>
-</div>
-</div>
-<div class="image">
-<img src="./public/images/articles/rogerfederer.jpg" alt="Roger Federer" width="500px" height="300px">
-</div>
-</div>
+<div class="articleView">
+  <div class="articleTitle">
+    <h1>Prince William and Prince Harry to stand vigil at Queen's coffin on Saturday<h1>
+  </div>
+  <div class="articleDateCategory">
+    <div class="articleDate">
+<em>12 Jun 2025</em>
+    </div>
+    <div class="articleCategory">
+<p>Entertainment</p>
+    </div>
+    </div>
+    <div class="articleImage">
+<img src="./public/images/articles/rogerfederer.jpg" alt="" width="50%" height="400px">
+    </div>
+    <div class="articleContent">
+      <p>
+      London (CNN)The late Queen Elizabeth II's eight grandchildren will stand vigil beside her coffin in Westminster Hall on Saturday evening, a royal source told CNN.
+
+      Prince William, the Prince of Wales, will stand at the head of the coffin, and Prince Harry, the Duke of Sussex, will stand at its foot for the 15-minute vigil, according to the source. At the King's request, both will be in uniform.
+
+      The Queen's other grandchildren will be wearing morning coats and dark formal dress with decorations, the royal source said.
+The source added that the Prince of Wales will be flanked by Zara Tindall and Peter Philips, who are the children of Princess Anne. The Duke of Sussex will be flanked by Princesses Beatrice and Eugenie, the daughters of Prince Andrew, alongside Prince Edward's children, Lady Louise Windsor and Viscount Severn.
+The grandchildren, at the King's invitation, are very eager to pay their respects -- just as their parents are doing the previous evening, the source told CNN.
+</p>
+    </div>
+    <div class="articleEditDelete">
+<p><a href="#">Edit</a></p>
+<p><a href="#">Delete</a></p>
+    </div>
+
 </div>
 
  <?php include 'footer.php' ?>
