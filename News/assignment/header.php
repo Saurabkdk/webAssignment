51230e13456c823272abdd4aae4cff9ab31f45ef
Saurabkdk <saurabkdk@gmail.com>
2022-09-16 22:04:54 +0545

    Adding login logout

--- News/assignment/header.php
+++ News/assignment/header.php
@@ -12,7 +12,14 @@
 		</header>
 		<nav>
 			<ul>
-				<li><a href="index.php">Latest Articles</a></li>
+				<?php
+				if (isset($_SESSION['adminLogin'])) {
+					echo '<li><a href="../article/adminArticles.php">Latest Articles</a></li>';
+				}
+				else {
+					echo '<li><a href="../index.php">Latest Articles</a></li>';
+				}
+				 ?>
 				<li><a href="#">Select Category</a>
 					<ul>
 						<li><a class="articleLink" href="#">Category 1</a></li>
@@ -20,6 +27,14 @@
 						<li><a class="articleLink" href="#">Category 3</a></li>
 					</ul>
 				</li>
+				<?php
+				if(isset($_SESSION['adminLogin']) || isset($_SESSION['userLogin'])){
+					echo '<li><a class="articleLink" href="../access/logout.php">Logout</a></li>';
+				}
+				else {
+					echo '<li><a class="articleLink" href="../access/login.php">Login</a></li>';
+				}
+				 ?>
 			</ul>
 		</nav>
 		<img src="../images/banners/randombanner.php" />
