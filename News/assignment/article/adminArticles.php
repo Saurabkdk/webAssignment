ef25126e08b1c31b75dcbfe451f26d8a2ccd02d3
Saurabkdk <saurabkdk@gmail.com>
2022-09-16 22:10:05 +0545

    Adding links

--- News/assignment/article/adminArticles.php
+++ News/assignment/article/adminArticles.php
@@ -3,27 +3,38 @@
 <link rel="stylesheet" href="../styles.css?v=<?php echo time(); ?>"/>
 
 <?php
-if ($_SESSION['adminLogin']) {
-
 include '../header.php';
 include 'articleTitleGenerator.php';
 include '../dbController/dbController.php';
+
+if (isset($_SESSION['adminLogin'])) {
+
  ?>
 
- <div>
-   <h1>Articles</h1>
+ <div class="articles">
+ <h1>Articles</h1>
+ <div class="optionButton">
+ <a href="addArticle.php"><button type="button">Add Article</button></a>
+ <a href="../category/adminCategories.php"><button type="button">View Categories</button></a>
+ <!-- <div class="options">
+ <a href="#">Add Article</a>
+ <a href="#">View Categories</a>
+ </div> -->
+ </div>
  </div>
 
 <?php
 $getAdminArticles = getAdminArticles();
 
+
 while ($adminArticles = $getAdminArticles -> fetch()) {
   $articleTitle = new articleTitleGenerator();
 
+  $articleTitle -> articleId = $adminArticles[0];
   $articleTitle -> articleTitle = $adminArticles[1];
   $articleTitle -> articleDate = $adminArticles[2];
   $articleTitle -> articleCategory = getCategoryName($adminArticles[3]);
-  if (strlen($adminArticles[4]) >0) {
+  if (strlen($adminArticles[4]) > 0) {
     $articleTitle -> articleImage = $adminArticles[4];
   }
   else {
@@ -36,6 +47,11 @@ while ($adminArticles = $getAdminArticles -> fetch()) {
  ?>
 
  <?php
+}
+else {
+  if(!isset($_SESSION['adminLogin'])){
+    redirect("../access/login.php");
+  }
 }
 include '../footer.php';
   ?>
