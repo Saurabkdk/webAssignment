d11a2682f2e5b15ea0018f8d1c1a0d5e15c56a9d
Saurabkdk <saurabkdk@gmail.com>
2022-09-19 00:09:55 +0545

    Adding like and comment options

--- News/assignment/article/article.php
+++ News/assignment/article/article.php
@@ -39,13 +39,46 @@ if (isset($_SESSION['adminLogin']) || isset($_SESSION['userLogin'])) {
         <?php echo $oneArticleContent ?>
       </div>
       <div class="articleEditDelete">
-        <p><a href="#">Edit</a></p>
-        <p><a href="#">Delete</a></p>
+        <p><a href="editArticle.php?id=<?php echo $_GET['id'] ?>">Edit</a></p>
+        <p><a href="deleteArticle.php?id=<?php echo $_GET['id'] ?>">Delete</a></p>
+      </div>
+
+      <div class="likeComment">
+
+        <div class="addComment">
+          <form class="" action="article.php?id=<?php echo $_GET['id']; ?>" method="post">
+            <textarea name="commentText" rows="8" cols="100" required></textarea>
+            <input style="margin : 1% 0; width: 100px;" type="submit" name="comment" value="Comment">
+          </form>
+        </div>
+        <div class="like">
+          <p><a href="../comment/like.php?id=<?php echo $_GET['id']; ?>">Like</a></p>
+        </div>
+
+      </div>
+
+      <h3 style="padding:1% 0 0 2%;">Comments</h3>
+
+      <div class="userComments">
+        <p>User</p>
+        <p class="comment">User Comment</p>
       </div>
 
     </div>
 
     <?php
+    if (isset($_POST['comment'])) {
+      $commentDetails = [validateFields($_POST['commentText']), $_SESSION['userId'], $_GET['id']];
+      if (addComment($commentDetails)) {
+        echo "Added";
+      }
+    }
+     ?>
+
+    <?php
+  }
+  else {
+    redirect('../access/login.php');
   }
   }
   include '../footer.php';
