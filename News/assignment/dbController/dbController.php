6ab5d6fbbc85bb57a5f60ee68b15045d5f769ca3
Saurabkdk <saurabkdk@gmail.com>
2022-09-19 00:16:22 +0545

    Adding necessary database functions

--- News/assignment/dbController/dbController.php
+++ News/assignment/dbController/dbController.php
@@ -8,17 +8,35 @@ $dbConnection = $dbConnect -> dbConnection();
 
 function loginAdmin($emailAdmin, $passwordAdmin){
   global $dbConnection;
-  $loginCheck = $dbConnection -> prepare('SELECT email, password FROM administrators WHERE email = :email');
+  $loginCheck = $dbConnection -> prepare('SELECT email, password, admin_id FROM administrators WHERE email = :email');
   $loginCheckValue =[
     'email' => $emailAdmin
   ];
   $loginCheck -> execute($loginCheckValue);
   if ($getAdmin = $loginCheck -> fetch()) {
     if (password_verify($passwordAdmin, $getAdmin[1])) {
-      return true;
+      return $getAdmin[2];
     }
     else {
-      redirect("Password did not match");
+      echo '<script>alert("Password did not match")</script>';
+    }
+  }
+  return false;
+}
+
+function loginUser($emailUser, $passwordUser){
+  global $dbConnection;
+  $loginCheck = $dbConnection -> prepare('SELECT email, password, user_id FROM users WHERE email = :email');
+  $loginCheckValue =[
+    'email' => $emailUser
+  ];
+  $loginCheck -> execute($loginCheckValue);
+  if ($getUser = $loginCheck -> fetch()) {
+    if (password_verify($passwordUser, $getUser[1])) {
+      return $getUser[2];
+    }
+    else {
+      echo '<script>alert("Password did not match")</script>';
     }
   }
   return false;
@@ -159,13 +177,167 @@ function getOneArticle($articleId){
   }
 }
 
+function editArticle($articleDetails){
+  global $dbConnection;
+  try {
+    $editArticle = $dbConnection -> prepare('UPDATE article SET title = :title, content = :content, categoryId = :categoryId, image = :image WHERE article_id = :articleId');
+    $editArticleValues = [
+      'title' => $articleDetails[0],
+      'content' => $articleDetails[1],
+      'categoryId' => $articleDetails[2],
+      'image' => $articleDetails[3],
+      'articleId' => $articleDetails[4]
+    ];
+    if($editArticle -> execute($editArticleValues)){
+      return true;
+    }
+  } catch (PDOException $exception) {
+    echo $exception -> getMessage();
+  }
+  return false;
+}
+
+function deleteArticle($articleId){
+  global $dbConnection;
+  try {
+    $deleteArticle = $dbConnection -> prepare('DELETE FROM article WHERE article_id = :articleId');
+    $deleteArticleValues = [
+      'articleId' => $articleId
+    ];
+    if($deleteArticle -> execute($deleteArticleValues)){
+      return true;
+    }
+  } catch (PDOException $exception) {
+    echo $exception -> getMessage();
+  }
+  return false;
+}
+
+function insertUser($userDetails){
+  global $dbConnection;
+  try {
+    $insertUser = $dbConnection -> prepare('INSERT INTO users VALUES (null, :email, :password, :name)');
+    $insertUserValues = [
+      'email' => "$userDetails[0]",
+      'password' => $userDetails[1],
+      'name' => $userDetails[2]
+    ];
+    if($insertUser -> execute($insertUserValues)){
+      return true;
+    }
+  } catch (PDOException $exception) {
+    echo $exception -> getMessage();
+  }
+  return false;
+}
+
+function addComment($commentDetails){
+  global $dbConnection;
+  try {
+    if (checkExists($commentDetails[1], $commentDetails[2])) {
+      $addComment = $dbConnection -> prepare('UPDATE comment SET comment = :comment WHERE user_id = :userId AND article_id = :articleId');
+      $addCommentValues = [
+        'comment' => $commentDetails[0],
+        'userId' => $commentDetails[1],
+        'articleId' => $commentDetails[2]
+      ];
+      if ($addComment -> execute($addCommentValues)) {
+        return true;
+      }
+    }
+    else {
+      $addComment = $dbConnection -> prepare('INSERT INTO comment VALUES (null, :comment, :userId, :articleId, :likePost)');
+      $addCommentValues = [
+        'comment' => $commentDetails[0],
+        'userId' => $commentDetails[1],
+        'articleId' => $commentDetails[2],
+        'likePost' => 0
+      ];
+      if ($addComment -> execute($addCommentValues)) {
+        return true;
+      }
+    }
+  } catch (PDOException $exception) {
+    echo $exception -> getMessage();
+  }
+  return false;
+}
+
+function addLike($likeDetails){
+  global $dbConnection;
+  try {
+    if (checkExists($likeDetails[1], $likeDetails[2])) {
+      $addLike = $dbConnection -> prepare('UPDATE comment SET likePost = :likePost WHERE user_id = :userId AND article_id = :articleId');
+      $addLikeValues = [
+        'likePost' => $likeDetails[0],
+        'userId' => $likeDetails[1],
+        'articleId' => $likeDetails[2]
+      ];
+      if ($addLike -> execute($addLikeValues)) {
+        return true;
+      }
+    }
+    else {
+      $addLike = $dbConnection -> prepare('INSERT INTO comment VALUES (null, null, :userId, :articleId, :likePost)');
+      $addLikeValues = [
+        'userId' => $likeDetails[1],
+        'articleId' => $likeDetails[2],
+        'likePost' => $likeDetails[0]
+      ];
+      if ($addLike -> execute($addLikeValues)) {
+        return true;
+      }
+    }
+  } catch (PDOException $exception) {
+    echo $exception -> getMessage();
+  }
+  return false;
+}
+
+function checkExists($userId, $articleId){
+  global $dbConnection;
+  try {
+    $checkExists = $dbConnection -> prepare('SELECT comment_id FROM comment WHERE user_id = :userId AND article_id = :articleId');
+    $checkExistsValue = [
+      'userId' => $userId,
+      'articleId' => $articleId
+    ];
+    $checkExists -> execute($checkExistsValue);
+    $recordCount = 0;
+    if ($record = $checkExists -> fetch()) {
+      $recordCount = $record[0];
+    }
+    if ($recordCount > 0) {
+      return true;
+    }
+  } catch (PDOException $exception) {
+    echo $exception -> getMessage();
+  }
+  return false;
+}
+
+function getUserId($emailUser){
+  global $dbConnection;
+  try {
+    $getUserId = $dbConnection -> prepare('SELECT user_id FROM users WHERE email = :emailUser');
+    $getUserIdValue = [
+      'emailUser' => $emailUser
+    ];
+    $getUserId -> execute($getUserIdValue);
+    $userId = $getUserId -> fetch();
+    return $userId[0];
+  } catch (PDOException $exception) {
+    echo $exception -> getMessage();
+  }
+}
+
 function insertAdmin($email, $password)
 {
   global $dbConnection;
   $adminInsert = $dbConnection -> prepare('INSERT INTO administrators VALUES (null, :adminEmail, :adminPassword)');
   $adminInsertValues = [
     'adminEmail' => $email,
-    'adminPassword' => password_hash($password, PASSWORD_DEFAULT)
+    'adminPassword' => $password
   ];
   if ($adminInsert -> execute($adminInsertValues)) {
     return true;
