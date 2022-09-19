6c282d7e96a53f7cd79d7e1b7145919401fc899d
Saurabkdk <saurabkdk@gmail.com>
2022-09-19 22:37:44 +0545

    Adding articles

--- News/assignment/index.php
+++ News/assignment/index.php
@@ -2,10 +2,16 @@
 session_start();
 ?>
 
+<?php
+include './dbController/dbController.php';
+include './article/articleTitleGenerator.php';
+include './access/validation.php';
+ ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="styles.css"/>
+		<link rel="stylesheet" href="../styles.css?v=<?php echo time(); ?>"/>
 		<title>Northampton News - Home</title>
 	</head>
 	<body>
@@ -18,10 +24,13 @@ session_start();
 			<ul>
 				<li><a href="index.php">Latest Articles</a></li>
 				<li><a href="#">Select Category</a>
-					<ul>
-						<li><a class="articleLink" href="#">Category 1</a></li>
-						<li><a class="articleLink" href="#">Category 2</a></li>
-						<li><a class="articleLink" href="#">Category 3</a></li>
+          <ul>
+						<?php
+		        $getCategory = getCategory();
+		        while($categoryList = $getCategory -> fetch()){
+		          echo '<li><a class="articleLink" href="./article/categoryArticles.php?id='. $categoryList[1] .'">'. $categoryList[0] .'</a></li>';
+		        }
+		        ?>
 					</ul>
 				</li>
 				<?php
@@ -35,9 +44,9 @@ session_start();
 			</ul>
 		</nav>
 		<img src="images/banners/randombanner.php" />
-		<main>
+		<main class="container">
 			<!-- Delete the <nav> element if the sidebar is not required -->
-			<nav>
+			<!-- <nav>
 				<ul>
 					<li><a href="#">Sidebar</a></li>
 					<li><a href="#">This can</a></li>
@@ -66,7 +75,40 @@ session_start();
 
 					<input type="submit" name="submit" value="Submit" />
 				</form>
-			</article>
+			</article> -->
+			<div class="articles">
+		  <h1>Articles</h1>
+		  <?php if (isset($_SESSION['adminLogin'])): ?>
+		    <div class="optionButton">
+		    <a href="addArticle.php"><button type="button">Add Article</button></a>
+		    <a href="../category/adminCategories.php"><button type="button">View Categories</button></a>
+		    </div>
+		  <?php endif; ?>
+		  </div>
+
+		 <?php
+		 $getAdminArticles = getAdminArticles();
+
+
+		 while ($adminArticles = $getAdminArticles -> fetch()) {
+		   $articleTitle = new articleTitleGenerator();
+
+			 $articleTitle -> articleLink = "./article/article.php?id=".$adminArticles[0];
+		   $articleTitle -> articleId = $adminArticles[0];
+		   $articleTitle -> articleTitle = $adminArticles[1];
+		   $articleTitle -> articleDate = $adminArticles[2];
+		   $articleTitle -> articleCategory = getCategoryName($adminArticles[3]);
+		   if (strlen($adminArticles[4]) > 0) {
+		     $articleTitle -> articleImage = $adminArticles[4];
+		   }
+		   else {
+		     $articleTitle -> articleImage = 'news.jpg';
+		   }
+		   echo $articleTitle -> generateArticleTitle();
+		 }
+
+
+		  ?>
 		</main>
 
 		<footer>
