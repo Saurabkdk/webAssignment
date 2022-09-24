70204c71de96bd1aea5680d820e073ce985f5e86
Saurabkdk <saurabkdk@gmail.com>
2022-09-24 16:24:30 +0545

    Used for testing

--- News/assignment/test.php
+++ News/assignment/test.php
@@ -1,7 +1,7 @@
-<link rel="stylesheet" href="../styles.css?v=<?php echo time(); ?>"/>
+<!-- <link rel="stylesheet" href="../styles.css?v=<?php echo time(); ?>"/>
 
 <?php
-include 'header.php';
+// include 'header.php';
  ?>
 
 <div class="articleView">
@@ -58,4 +58,82 @@ The grandchildren, at the King's invitation, are very eager to pay their respect
 
 </div>
 
- <?php include 'footer.php' ?>
+ <?php //include 'footer.php' ?> -->
+
+ <?php
+ session_start();
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ 	<head>
+ 		<link rel="stylesheet" href="styles.css"/>
+ 		<title>Northampton News - Home</title>
+ 	</head>
+ 	<body>
+ 		<header>
+ 			<section>
+ 				<h1>Northampton News</h1>
+ 			</section>
+ 		</header>
+ 		<nav>
+ 			<ul>
+ 				<li><a href="index.php">Latest Articles</a></li>
+ 				<li><a href="#">Select Category</a>
+ 					<ul>
+ 						<li><a class="articleLink" href="#">Category 1</a></li>
+ 						<li><a class="articleLink" href="#">Category 2</a></li>
+ 						<li><a class="articleLink" href="#">Category 3</a></li>
+ 					</ul>
+ 				</li>
+ 				<?php
+ 				if(isset($_SESSION['adminLogin']) || isset($_SESSION['userLogin'])){
+ 					echo '<li><a class="articleLink" href="./access/logout.php">Logout</a></li>';
+ 				}
+ 				else {
+ 					echo '<li><a class="articleLink" href="./access/login.php">Login</a></li>';
+ 				}
+ 				 ?>
+ 			</ul>
+ 		</nav>
+ 		<img src="images/banners/randombanner.php" />
+ 		<main>
+ 			<!-- Delete the <nav> element if the sidebar is not required -->
+ 			<nav>
+ 				<ul>
+ 					<li><a href="#">Sidebar</a></li>
+ 					<li><a href="#">This can</a></li>
+ 					<li><a href="#">Be removed</a></li>
+ 					<li><a href="#">When not needed</a></li>
+ 				</ul>
+ 			</nav>
+
+ 			<article>
+ 				<h2>A Page Heading</h2>
+ 				<p>Text goes in paragraphs</p>
+
+ 				<ul>
+ 					<li>This is a list</li>
+ 					<li>With multiple</li>
+ 					<li>List items</li>
+ 				</ul>
+
+
+ 				<form>
+ 					<p>Forms are styled like so:</p>
+
+ 					<label>Field 1</label> <input type="text" />
+ 					<label>Field 2</label> <input type="text" />
+ 					<label>Textarea</label> <textarea></textarea>
+
+ 					<input type="submit" name="submit" value="Submit" />
+ 				</form>
+ 			</article>
+ 		</main>
+
+ 		<footer>
+ 			&copy; Northampton News 2017
+ 		</footer>
+
+ 	</body>
+ </html>
